f7e78dac80732fd10c55f116e20fa8c8df41ca7a
weiling920131 <weiling920131@gmail.com>
2023-10-06 16:19:40 +0800

    cannot fix video.py

--- Lab02/1.py
+++ Lab02/1.py
@@ -1,25 +1,19 @@
 import cv2
 import numpy as np
 
-# img = cv2.imread('images/12.png')
-
-# img = cv2.imread(r"C:\Users\user\Downloads\2.jpg")
 img = cv2.imread('images/filtering.jpg')
-# img = cv2.imread('images/1.jpg')
-# img = cv2.imread('images/1.png')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img = cv2.GaussianBlur(img, (5,5), 0)
 
 x_kernel = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
 y_kernel = x_kernel.T
-gx = cv2.filter2D(img, -1, x_kernel)
-gy = cv2.filter2D(img, -1, y_kernel)
+gx = np.array(cv2.filter2D(img, -1, x_kernel), dtype=np.int32)
+gy = np.array(cv2.filter2D(img, -1, y_kernel), dtype=np.int32)
 
-# new_img = np.array(gx + gy)
-new_img = np.array(abs(gx) + abs(gy))
-# new_img = np.around(np.sqrt(gx**2 + gy**2))
-# new_img = np.sqrt(np.square(gx) + np.square(gy))
-# new_img *= 255.0 / new_img.max()
-cv2.imwrite('output/1.png', new_img.astype(np.float32))
-# img = cv2.Sobel(img, -1, 1, 1, 7)
-# cv2.imwrite('output/1.png', img.astype(np.float32))
+new_img = np.array((abs(gx) + abs(gy)), dtype=np.int32)
+np.clip(new_img, 0, 255, out=new_img)
+new_img = np.array(new_img, dtype=np.uint8)
+
+cv2.imwrite('output/1.png', new_img)
+cv2.imshow('1.png', new_img)
+cv2.waitKey(0)
---
+++ Lab03/test.py
@@ -0,0 +1,133 @@
+import cv2
+import numpy as np
+import os
+
+def select_object(frame, threshold, resolve, areas, cc_mask):
+    frame = np.array(frame, dtype=np.int32)
+    for set in resolve:
+        area = 0
+        for label in set:
+            area += areas[label]
+        
+        if area > threshold and area < 10000:
+            x_array, y_array = np.where(np.isin(cc_mask, np.array(list(set)))) ##notice
+            
+            cv2.rectangle(frame, (x_array[0], y_array[0]), (int(x_array[0]+area**0.52), int(y_array[0]+area**0.48)), (0, 255, 0), 2)
+            
+    np.clip(frame, 0, 255, out=frame)
+    frame = np.array(frame, dtype=np.uint8)
+    return frame
+
+        
+def connected_component(nmask):
+    h, w = nmask.shape
+    resolve = []
+    areas = dict()
+    new_mask = np.zeros_like(nmask, dtype=np.int32)
+    unused = 2
+
+    for i in range(h):
+        for j in range(w):
+            if nmask[i][j] == 0: 
+                continue
+
+            if not i and not j:
+                new_mask[i][j] = 1
+                areas[1] = 1
+            elif not i:
+                if nmask[i][j-1] != 0:
+                    new_mask[i][j] = new_mask[i][j-1]
+                    areas[new_mask[i][j]] += 1
+                else:
+                    new_mask[i][j] = unused
+                    areas[unused] = 1
+                    unused += 1
+            elif not j:
+                if nmask[i-1][j] != 0:
+                    new_mask[i][j] = new_mask[i-1][j]
+                    areas[new_mask[i][j]] += 1
+                else:
+                    new_mask[i][j] = unused
+                    areas[unused] = 1
+                    unused += 1
+            else:
+                if nmask[i-1][j] != 0 and nmask[i][j-1] != 0:
+                    new_mask[i][j] = min(new_mask[i-1][j], new_mask[i][j-1])
+                    areas[new_mask[i][j]] += 1
+                    check = True
+                    for set in resolve:
+                        if new_mask[i][j] in set:
+                            set.add(max(new_mask[i-1][j], new_mask[i][j-1]))
+                            check = False
+                            break
+                    if check:
+                        resolve.append({new_mask[i-1][j], new_mask[i][j-1]})
+
+                elif nmask[i-1][j] != 0:
+                    new_mask[i][j] = new_mask[i-1][j]
+                    areas[new_mask[i][j]] += 1
+                elif nmask[i][j-1] != 0:
+                    new_mask[i][j] = new_mask[i][j-1]
+                    areas[new_mask[i][j]] += 1
+                else:
+                    new_mask[i][j] = unused
+                    areas[unused] = 1
+                    unused += 1
+    return resolve, areas, new_mask
+
+
+def solve(video, threshold):
+    backSub = cv2.createBackgroundSubtractorMOG2()
+    while True:
+        ret, frame = video.read()
+        if not ret:
+            break
+        
+        fgmask = backSub.apply(frame) ##find foreground
+        shadowval = backSub.getShadowValue() ## elminiate shadow
+        ret, nmask = cv2.threshold(fgmask, shadowval, 255, cv2.THRESH_BINARY)
+        if not ret:
+            break
+        
+        # cv2.imshow("frame", nmask)
+        # if cv2.waitKey(33) & 0xFF == ord('q'):
+        #     break
+
+        # resolve, areas, cc_mask = connected_component(nmask)
+        # selected_frame = select_object(frame, threshold, resolve, areas, cc_mask)
+
+        cv2.imshow("frame", nmask)
+
+        # cv2.circle(frame, (50, 50), 10, (255,0,0), 5)
+        # cv2.circle(frame, (200, 200), 10, (0,255,0), 5)
+        # cv2.imshow("frame", frame)
+        if cv2.waitKey(33) & 0xFF == ord('q'):
+            break
+
+if __name__ == '__main__':
+
+    video = cv2.VideoCapture('input/train.mp4')
+    if not video.isOpened():
+        print('error : The video is not found.')
+        exit(1)
+        
+    # minutes = 0
+    # seconds = 30
+    # frame_to_capture = int(minutes * 60 * video.get(cv2.CAP_PROP_FPS) + seconds * video.get(cv2.CAP_PROP_FPS))
+
+    # # 设置帧号
+    # video.set(cv2.CAP_PROP_POS_FRAMES, frame_to_capture)
+    # ret, frame = video.read()
+    # cv2.imwrite('input/3.jpg', frame)
+
+    solve(video, threshold=1000)
+    # test = np.array([[0,0,0,0,0,255,255,255,0,0],
+    #                  [255,255,255,0,0,255,255,0,0,0],
+    #                  [255,0,0,255,255,0,0,0,255,255],
+    #                  [255,0,0,255,255,0,0,0,255,255]])
+    # resolve, areas, cc_mask = connected_component(test)
+    # print(resolve)
+    # print('++++++++++++++++++')
+    # print(areas)
+    # print('++++++++++++++++++')
+    # print(cc_mask)
