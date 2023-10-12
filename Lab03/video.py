df24a5ad502e03842355cfd2543aef02cf48f62a
weiling920131 <weiling920131@gmail.com>
2023-10-12 14:15:54 +0800

    optimize lab03

--- Lab03/video.py
+++ Lab03/video.py
@@ -5,24 +5,56 @@ import os
 def select_object(frame, threshold, resolve, areas, cc_mask):
     h, w, _ = frame.shape
     frame = np.array(frame, dtype=np.int32)
-    for set in resolve:
-        area = 0
-        for label in set:
-            area += areas[label]
+    
+    for i in range(h):
+        for j in range(w):
+            if not cc_mask[i][j]:
+                continue
+            for set in resolve:
+                if cc_mask[i][j] in set:
+                    cc_mask[i][j] = min(set)
+    tmp = dict()
+    for i in range(h):
+        for j in range(w):
+            if cc_mask[i][j] in tmp:
+                tmp[cc_mask[i][j]] += 1
+            else:
+                tmp[cc_mask[i][j]] = 1
+    for label, area in tmp.items():
+        if area < threshold or area > 10000:
+            continue
+        right = 0
+        left = w
+        top = 0
+        bottom = h
+        for i in range(h):
+            for j in range(w):
+                if label == cc_mask[i][j]:
+                    right = max(right, j)
+                    left = min(left, j)
+                    top = max(top, i)
+                    bottom = min(bottom, i)
+        cv2.rectangle(frame, (right, top), (left, bottom), (0, 255, 0), 2)
+
+
+    # for set in resolve:
+    #     area = 0
+    #     for label in set:
+    #         area += areas[label]
         
-        if area > threshold:
-            right = 0
-            left = w
-            top = 0
-            bottom = h
-            for i in range(h):
-                for j in range(w):
-                    if cc_mask[i][j] in set:
-                        right = max(right, j)
-                        left = min(left, j)
-                        top = max(top, i)
-                        bottom = min(bottom, i)
-            cv2.rectangle(frame, (right, top), (left, bottom), (0, 255, 0), 2)
+    #     if area > threshold:
+    #         right = 0
+    #         left = w
+    #         top = 0
+    #         bottom = h
+    #         for i in range(h):
+    #             for j in range(w):
+    #                 if cc_mask[i][j] in set:
+    #                     right = max(right, j)
+    #                     left = min(left, j)
+    #                     top = max(top, i)
+    #                     bottom = min(bottom, i)
+    #         cv2.rectangle(frame, (right, top), (left, bottom), (0, 255, 0), 2)
             
     np.clip(frame, 0, 255, out=frame)
     frame = np.array(frame, dtype=np.uint8)
@@ -98,23 +130,15 @@ def solve(video, threshold):
         ret, nmask = cv2.threshold(fgmask, shadowval, 255, cv2.THRESH_BINARY)
         if not ret:
             break
-        
-        # cv2.imshow("frame", nmask)
-        # if cv2.waitKey(33) & 0xFF == ord('q'):
-        #     break
+        x, y = nmask.shape
+        if np.count_nonzero(nmask == 255) < x*y*0.1:
 
-        resolve, areas, cc_mask = connected_component(nmask)
-        # np.set_printoptions(threshold=np.inf)
-        # print(cc_mask)
-        selected_frame = select_object(frame, threshold, resolve, areas, cc_mask)
-
+            resolve, areas, cc_mask = connected_component(nmask)
+            selected_frame = select_object(frame, threshold, resolve, areas, cc_mask)
+        else:
+            selected_frame = frame
         cv2.imshow("frame", selected_frame)
-
-        # cv2.circle(frame, (50, 50), 10, (255,0,0), 5)
-        # cv2.circle(frame, (200, 200), 10, (0,255,0), 5)
-        # cv2.imshow("frame", frame)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(33) & 0xFF == ord(']'):
             break
 
 if __name__ == '__main__':
@@ -123,24 +147,5 @@ if __name__ == '__main__':
     if not video.isOpened():
         print('error : The video is not found.')
         exit(1)
-        
-    # minutes = 0
-    # seconds = 30
-    # frame_to_capture = int(minutes * 60 * video.get(cv2.CAP_PROP_FPS) + seconds * video.get(cv2.CAP_PROP_FPS))
 
-    # # 设置帧号
-    # video.set(cv2.CAP_PROP_POS_FRAMES, frame_to_capture)
-    # ret, frame = video.read()
-    # cv2.imwrite('input/3.jpg', frame)
-
-    solve(video, threshold=700)
-    # test = np.array([[0,0,0,0,0,255,255,255,0,0],
-    #                  [255,255,255,0,0,255,255,0,0,0],
-    #                  [255,0,0,255,255,0,0,0,255,255],
-    #                  [255,0,0,255,255,0,0,0,255,255]])
-    # resolve, areas, cc_mask = connected_component(test)
-    # print(resolve)
-    # print('++++++++++++++++++')
-    # print(areas)
-    # print('++++++++++++++++++')
-    # print(cc_mask)
+    solve(video, threshold=450)
