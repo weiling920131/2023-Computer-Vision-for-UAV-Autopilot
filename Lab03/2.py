bf20399569ee96197d1d7ac42e89a8fd4b2f5b48
rebeccasun31 <rebeccasun31@gmail.com>
2023-10-16 17:39:11 +0800

    fin QQ

--- Lab03/2.py
+++ Lab03/2.py
@@ -4,6 +4,9 @@ import numpy as np
 cap = cv2.VideoCapture('images/train.mp4')
 backSub = cv2.createBackgroundSubtractorMOG2()
 
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('output/2.mp4', fourcc, 20.0, (320, 240))
+
 while cap.isOpened():
     ret, frame = cap.read()
     if frame is None:
@@ -15,35 +18,57 @@ while cap.isOpened():
     ret, nmask = cv2.threshold(fgmask, shadowval, 255, cv2.THRESH_BINARY)
 
     h, w = nmask.shape
-    label = np.zeros((h, w))
-    equal = np.array([[None] * (h * w)]) 
-    # break
+    label = np.zeros((h, w), dtype=int)
+    cur_label = 0
+    equal = {}
     for i in range(h):
         for j in range(w):
-            if label[i, j] == 0 and nmask[i, j] == 255:
-                if i == 0 and j == 0:
-                    label[i, j] = 1
-                elif i == 0:
-                    if label[i, j-1] != 0:
-                        label[i, j] = label[i, j-1]
-                    else:
-                        label[i, j] = 1
-                elif j == 0:
-                    if label[i-1, j] != 0:
-                        label[i, j] = label[i-1, j]
-                    else:
-                        label[i, j] = 1
-                else:
-                    if label[i, j-1] != 0 and label[i-1, j] != 0:
-                        label[i, j] = label[i, j-1]
-                        if label[i, j-1] != label[i-1, j]:
-                            equal[label[i, j-1]] += label[i-1, j]
-                            equal[label[i-1, j]] += label[i, j-1]
+            if nmask[i, j] == 255:
+                neighbor = []
+                if i != 0 and label[i-1, j] != 0:
+                    neighbor += [label[i-1, j]]
+                if j != 0 and label[i, j-1] != 0:
+                    neighbor += [label[i, j-1]]
 
+                n = len(neighbor)
+                if n == 0:
+                    cur_label += 1
+                    label[i, j] = cur_label
+                elif n == 1:
+                    label[i, j] = neighbor[0]
+                elif neighbor[0] != neighbor[1]:
+                    label[i, j] = min(neighbor)
+                    equal[neighbor[0]] = equal[neighbor[0]] + [neighbor[1]] if neighbor[0] in equal else [neighbor[1]]
+                    equal[neighbor[1]] = equal[neighbor[1]] + [neighbor[0]] if neighbor[1] in equal else [neighbor[0]]
 
-    cv2.imshow("frame", frame)
-    cv2.imshow("mask", nmask)
-    cv2.waitKey(33)
+    # print(equal)      
+    area = {}
+    for i in range(h):
+        for j in range(w):
+            l = label[i, j]
+            if l != 0:
+                while l in equal:
+                    if min(equal[l]) > l:
+                        break
+                    l = min(equal[l])
+                label[i, j] = l
+                area[l] = area[l] + [[i, j]] if l in area else [[i, j]]
+
+    # print(area)
+    T = 400
+    for i in area:
+        if len(area[i]) > T:
+            a = np.array(area[i]).T
+            h1 = min(a[0])
+            w1 = min(a[1])
+            h2 = max(a[0])
+            w2 = max(a[1])
+            cv2.rectangle(frame, (w1, h1), (w2, h2), (0, 0, 255), 3, cv2.LINE_AA)
+    
+    out.write(frame)
+    # cv2.imshow("frame", frame)
+    # cv2.waitKey(33)
 
 cap.release()
+out.release()
 cv2.destroyAllWindows()
